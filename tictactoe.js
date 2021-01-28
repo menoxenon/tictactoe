858e1df82229f6489ab3ea7d3220c5408b27ba1d
menoxenon <49659710+menoxenon@users.noreply.github.com>
2021-01-28 14:05:07 +0100

    first_version

---
+++ tictactoe.js
@@ -0,0 +1,152 @@
+var player1 = "Player 1" //o
+    var player2 = "Player 2" //x
+    var counter = 0
+    counter = counter * 1
+
+    var XScore = 0
+    var OScore = 0
+
+
+    var ticValues = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    var ticTab = ["number1", "number2", "number3", "number4", "number5", "number6", "number7", "number8", "number9"]
+
+    function whatIsUrName() {
+        player1 = prompt("Set first player name:", "")
+        player2 = prompt("Set second player name:", "")
+    }
+
+    function ticSelect(s) {
+        var cell = s / 1
+
+        if (ticValues[cell] == 0) {
+            xOrO(counter, s)
+            counter++
+        } else {
+            alert("Select another cell!");
+        }
+
+        winCheck();
+    }
+
+    function xOrO(y, which) {
+        if (y % 2 == 0) {
+            document.getElementById("nowplaying").innerHTML = player2
+            ticValues[which] = 1
+            inserto(which)
+        } else {
+            document.getElementById("nowplaying").innerHTML = player1
+            ticValues[which] = 2
+            insertx(which)
+        }
+
+        counter = counter % 2
+    }
+
+    function insertx(x) {
+        document.getElementById(ticTab[x]).innerHTML = '<i class="fas fa-times"></i>'
+    }
+
+    function inserto(o) {
+        document.getElementById(ticTab[o]).innerHTML = '<i class="fas fa-circle"></i>'
+    }
+
+    function ticReset() {
+        for (i = 0; i < 9; i++) {
+            document.getElementById(ticTab[i]).innerHTML = ""
+        }
+
+        for (i = 0; i < 9; i++) {
+            ticValues[i] = 0
+        }
+
+        XScore = 0;
+        OScore = 0;
+
+        document.getElementById("scorex").innerHTML = "000"
+        document.getElementById("scoreo").innerHTML = "000"
+
+        counter = 0;
+
+        whatIsUrName();
+    }
+
+    function winCheck() {
+        for (i = 1; i < 4; i++) {
+            if (ticValues[(i * 3) - 3] == ticValues[(i * 3) - 2] && ticValues[(i * 3) - 2] == ticValues[(i * 3) - 1]) {
+                if (ticValues[(i * 3) - 3] == 0) {
+                    break
+                } else {
+                    ticWin((i * 3) - 3)
+                }
+            }
+        } //linecheck landscape
+
+        for (i = 1; i < 4; i++) {
+            if (ticValues[(i - 1)] == ticValues[(i + 2)] && ticValues[(i + 2)] == ticValues[(i + 5)]) {
+                if (ticValues[i + 5] == 0) {
+                    break
+                } else {
+                    ticWin(i + 5)
+                }
+            }
+        }  //linecheck portrait
+
+        if (ticValues[0] == ticValues[4] && ticValues[4] == ticValues[8]) {
+            ticWin(8)
+        }
+
+        if (ticValues[2] == ticValues[4] && ticValues[4] == ticValues[6]) {
+            ticWin(6)
+        }
+
+        for (i = 0; i < 9; i++) {
+            if (ticValues[i] == 0) {
+                break;
+            } else if (i == 8 || ticValues[8] !== 0) {
+                cler()
+            }
+        }
+    }
+
+    function ticWin(w) {
+        if (ticValues[w] == 1) {
+            winO()
+        } else if (ticValues[w] == 2) {
+            winX()
+        }
+    }
+
+    function winX() {
+        XScore++;
+        if (XScore < 100) {
+            XScore = "0" + XScore;
+            if (XScore < 10) {
+                XScore = "0" + XScore;
+            }
+        }
+        document.getElementById("scorex").innerHTML = XScore;
+        cler();
+    }
+
+    function winO() {
+        OScore++;
+        if (OScore < 100) {
+            OScore = "0" + OScore;
+            if (OScore < 10) {
+                OScore = "0" + OScore;
+            }
+        }
+        document.getElementById("scoreo").innerHTML = OScore;
+        cler();
+    }
+
+    function cler() {
+        for (i = 0; i < 9; i++) {
+            document.getElementById(ticTab[i]).innerHTML = "";
+        }
+
+        for (i = 0; i < 9; i++) {
+            ticValues[i] = 0;
+        }
+    }
